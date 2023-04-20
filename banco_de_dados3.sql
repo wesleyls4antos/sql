f262afed4b38c57a3b2ed7ce50cf89f5380b6f2c
Wesley Rafael <rafaelwesley707@gmail.com>
2023-04-20 10:21:10 -0300

    Update banco_de_dados3.sql

--- banco_de_dados3.sql
+++ banco_de_dados3.sql
@@ -1,15 +1,12 @@
-SELECT matricula, nome, cpf FROM aluno WHERE matricula = "202300006"
-
-SELECT * FROM curso WHERE sigla = "JDI"
-
-SELECT * FROM curso WHERE sigla = "CCO" or sigla = "JDI"
-
-SELECT * FROM curso, aluno WHERE nome = "Bruce Wayne" AND sigla = "STI"
-
-
-SELECT * FROM aluno_curso, aluno WHERE matricula = "202300001"
-
-SELECT nome_curso, matricula, nome, cpf FROM aluno_curso
+ SELECT nome_curso, matricula, nome, cpf FROM aluno_curso
 INNER JOIN aluno ON aluno = matricula 
 INNER JOIN curso ON curso = cod_curso 
+WHERE matricula = "202300001" 
+
+SELECT nome, matricula, nome_curso, nome_disciplina FROM aluno_curso 
+INNER JOIN aluno ON aluno = matricula 
+INNER JOIN curso ON curso = cod_curso 
+INNER JOIN aluno_turma ON aluno_turma.aluno = matricula
+INNER JOIN turma ON aluno_turma.turma = cod_turma
+INNER JOIN disciplina ON turma.disciplina = cod_disciplina 
 WHERE matricula = "202300001" 
