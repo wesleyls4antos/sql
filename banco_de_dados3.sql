0264d4a203ef0aa36b562e912b7a9df9339f982f
Wesley Rafael <rafaelwesley707@gmail.com>
2023-04-27 10:29:31 -0300

    Update banco_de_dados3.sql

--- banco_de_dados3.sql
+++ banco_de_dados3.sql
@@ -10,3 +10,10 @@ INNER JOIN aluno_turma ON aluno_turma.aluno = matricula
 INNER JOIN turma ON aluno_turma.turma = cod_turma
 INNER JOIN disciplina ON turma.disciplina = cod_disciplina 
 WHERE matricula = "202300001" 
+
+UPDATE curso SET nome_curso = 'Redes de computadores' WHERE  cod_curso = 7
+
+DELETE FROM curso WHERE cod_curso = 7
+
+DELETE FROM curso WHERE cod_curso = 1
+SET foreign_key_checks = 1
