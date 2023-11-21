20a8115a6e5485fdf2f1d0e6e8293e5c35ea83a9
Wesley Rafael <rafaelwesley707@gmail.com>
2023-11-21 11:23:03 -0300

    Delete wesley.r directory

--- wesley.r/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$conexao = mysqli_connect('localhost', 'root', '', 'seguranca_1', '3306');
-if (!$conexao) {
-    die('Não foi possível conectar');
-}
-
-?>
--- wesley.r/funcoes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function criptografar($valor) {
-    return md5($valor);
-}
-
-
-?>
